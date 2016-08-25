a05e9bf658a604f4f2dea6dff90a32e40ec6fdb0
Tomas Vojta <tomas.vojtaaa@gmail.com>
2016-08-25 11:09:26 +0200

    prvni upload

--- multiplikace-penez.php
+++ multiplikace-penez.php
@@ -1,8 +1,8 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Multiplikace peněz - kalkulačka</title>
+<title>Multiplikace peněz - kalkulačka </title>
 <meta name="description" content="Odkazy a kalkulačka multiplikace peněz"> 
 
 </head>
